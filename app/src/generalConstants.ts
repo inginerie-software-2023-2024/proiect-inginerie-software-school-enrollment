052ab74cdd7b0a2d699d01ccea7f2ea27ab22ba8
imcatag <imcatag@gmai.com>
2024-01-30 15:34:44 +0200

    fix wrong domain name

--- app/src/generalConstants.ts
+++ app/src/generalConstants.ts
@@ -1,4 +1,4 @@
-export const domainName = "http://localhost:8080"
+export const domainName = "http://catag.go.ro:8080"
 export const romanianNameRegex = /^([A-ZĂÎÂȘȚ]([a-zA-ZăîâșțĂÎÂȘȚ])*([-. ])*)+$/
 export const romanianCNPRegex = /^[56][0-9]{12}$/
 export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
